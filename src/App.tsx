551522c4fb86fe9b3989c5bb02a1ea155634f462
Daniel Barocio <danybarociov@gmail.com>
2022-06-13 15:38:10 -0700

    Preparacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-function App() {
+function App(props: any) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-full w-full">
+      <div className="w-4/5 m-auto py-5 space-y-10 max-w-screen-2xl">
+        <div className="uppercase text-amber-400 font-bold p-3 bg-white border-2 border-black text-3xl rounded-xl w-fit">
+          Pantallas
+        </div>
+        <div className="flex flex-col space-y-10 m-auto">
+          {props.screens.map((screen: any, index: any) => {
+            return (
+              <Link
+                to={screen}
+                className=" border-2 border-black rounded-lg px-10 py-5 hover:shadow-lg hover:bg-amber-500 hover:text-white duration-200 font-bold"
+              >
+                {screen}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const screens = [
+  "Inicio",
+  "Productos",
+  "Registro",
+  "Productos",
+  "Registro",
+  "Cuenta",
+  "Laboratorios",
+  "Carrito",
+  "EditarPerfil",
+  "Producto",
+  "Laboratorio",
+  "EditarProducto",
+  "AdminItems",
+  "FormularioLaboratorio",
+];
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<App screens={screens} />} />
+      {screens.map((screen, index) => {
+        return (
+          <Route
+            key={index}
+            path={`/${screen}`}
+            element={`<div>${screen}</div>}`}
+          />
+        );
+      })}
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
