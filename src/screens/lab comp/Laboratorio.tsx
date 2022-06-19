862aedd2dd026348144400f72e881d8bf3d1bd0e
Osminalx <verdestro64@gmail.com>
2022-06-19 09:45:23 -0700

    Comienzo

--- src/screens/lab comp/Laboratorio.tsx
+++ src/screens/lab comp/Laboratorio.tsx
@@ -2,20 +2,43 @@ import { useState } from "react"
 
 
 const Laboratorio = () => {
-    const [Lab, setLab] = useState([
+
+    interface lab {
+        Nombre: String
+        id: number
+        img: String
+        Descripcion?: String
+    }
+
+    const INITIAL_STATE = [
+
         {
             Nombre: "Fotografia",
             id: 1,
-            img: "../../img/camara.jpg"
+            img: "../../img/camara.jpg",
+            Descripcion: "En este laboratorio hay cámaras"
+        },
+        {
+            Nombre: "Almacen",
+            id: 2,
+            img: "../../img/tijeras.png"
         }
-    ])
-    return (
-        <div>
-            {/* <h3>{Lab.Nombre}</h3> */}
-            <h5></h5>
-            <button></button>
+    ]
 
-        </div>
+    const [Lab, setLab] = useState<lab[]>([
+    ])
+
+    setLab(INITIAL_STATE)
+
+    return (
+        Lab.map(Lab => {
+            <div>
+                <h3>{Lab.Nombre}</h3>
+                <h5>{Lab.Descripcion}</h5>
+                <button></button>
+
+            </div>
+        })
     )
 }
 
