1f7c33b10a41a871f2490d2b106049baf9357d34
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-06-28 18:51:23 -0700

    solo hice bold unas letras

--- src/screens/Inicio.tsx
+++ src/screens/Inicio.tsx
@@ -10,7 +10,7 @@ const Inicio = () => {
       </div>
       <div className="h-[400px] flex flex-row grid-cols-2">
         <div className="w-[50%] flex flex-col space-y-1 ">
-          <h1 className="text-6xl mt-8 mb-4 mx-16 text-left">RackDAT</h1>
+          <h1 className="mt-8 mb-4 mx-16 text-left font-bold text-6xl">RackDAT</h1>
           <p className="text-2xl mx-8 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nesciunt neque excepturi similique, porro fugiat distinctio libero voluptas quibusdam repellat, tenetur totam est tempora minima facilis iste. Recusandae numquam tempore earum tenetur rem quam rerum et debitis dolorem asperiores amet ullam exercitationem minus suscipit odit, voluptas neque? Ipsum, et ad. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quia natus voluptatem aspernatur labore laborum doloribus rem repellendus. Provident, aperiam?</p>
         </div>
         <div className="w-[50%] flex justify-center items-center">
@@ -22,7 +22,7 @@ const Inicio = () => {
           <img src={tijeras} alt="" className="h-[80%] w-[90%] object-cover" />
         </div>
         <div className="w-[50%] flex flex-col space-y-1">
-          <h1 className="text-6xl mt-8 mb-4 mx-16 text-right">Préstamo</h1>
+          <h1 className="text-6xl mt-8 mb-4 mx-16 text-right font-bold">Préstamo</h1>
           <p className="text-2xl mx-8 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor minus quas doloremque, voluptatum nihil quis ipsum iusto odio voluptas deleniti ex similique modi corrupti laborum, odit ab repudiandae vero molestias? Sequi dicta neque labore libero cupiditate soluta quod alias pariatur veniam, id tempore sed, impedit temporibus provident sapiente nobis iusto nam explicabo eos eveniet vitae! Quos, labore ab fuga dolorum quisquam tempore ad velit saepe sunt, similique aliquid nisi distinctio.</p>
         </div>
       </div>
